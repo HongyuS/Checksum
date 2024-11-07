60426daa705f2de454df47c07d72061c4ea7eb12
Hongyu Shi <44023928+HongyuS@users.noreply.github.com>
2024-11-07 15:40:29 +0800

    Initial Commit

---
+++ Checksum/ChecksumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecksumApp.swift
+//  Checksum
+//
+//  Created by Hongyu Shi on 2024/11/7.
+//
+
+import SwiftUI
+
+@main
+struct ChecksumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChecksumTests/ChecksumTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChecksumTests.swift
+//  ChecksumTests
+//
+//  Created by Hongyu Shi on 2024/11/7.
+//
+
+import Testing
+
+struct ChecksumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
